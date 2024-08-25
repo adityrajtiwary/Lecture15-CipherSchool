6c63aa3b748174758d5d388e17aef820f6e4574c
adityrajtiwary <122718170+adityrajtiwary@users.noreply.github.com>
2024-08-25 09:25:40 +0530

    Add files via upload

---
+++ Lecture15-CipherSchool.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+//int compare(int a, int b)
+//{
+//	if(a>b)
+//	{
+//		c = a;
+//	}
+//	else{
+//		c = b;
+//	}
+//	return c;
+//}
+
+//int compare(int a, int b)
+//{
+//	if(a>b)
+//	{
+//		return a;
+//	}
+//	else{
+//		return b;
+//	}
+//}
+
+//int compare(int a, int b)
+//{
+//	if(a==5)
+//	{
+//		return 1;
+//	}
+//	else{
+//		return 0;
+//	}
+//}
+
+int main()
+{
+	int a, b ;
+	a = 5;
+	b = 3;
+	
+	cout<<compare(a,b);
+	return 0;
+}
